8d4839118d3e14bafac0d797d53f5f481ca4b021
Pedro Nieto <pedronieto.2005@gmail.com>
2022-07-11 20:06:21 -0300

    Update Parâmetros.sh

--- Parâmetros.sh
+++ Parâmetros.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Bom dia! Qual é o seu nome?"
-nome=$(whoami)
-echo ""
-echo "Olá, $nome"
+echo "Bom dia! Qual é o seu nome?" #Printa o texto entre parênteses
+nome=$(whoami) #Cria uma variável chamada "nome" que tem como valor o comando "whoami"
+echo "" #Printa o texto em parênteses
+echo "Olá, $nome" #Printa o texto em parênteses junto da variável "nome"
 
 #Enviar parâmetros para o script faz com que as variáveis sejam fixas, não podendo ser definidas na hora pelo usuário do script.
