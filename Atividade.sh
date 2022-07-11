c00e58a45e1427eb6a9480fed367eb520320c3c2
Pedro Nieto <pedronieto.2005@gmail.com>
2022-07-11 20:00:20 -0300

    Update Atividade.sh

--- Atividade.sh
+++ Atividade.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-clear
-echo "Seu nome de usuário é:"
-whoami
-echo ""
+clear //Limpa o terminal
+echo "Seu nome de usuário é:" //Printa o texto entre parênteses
+whoami //Printa o nome de usuário
+echo "" //Printa o texto entre parênteses
 
-echo "Info de dia e hora atual:"
-date
-echo ""
+echo "Info de dia e hora atual:" //Printa o texto entre parênteses
+date //Printa a data e a hora
+echo "" //Printa o texto entre parênteses
 
-echo "O script está executando do diretório:"
-pwd
+echo "O script está executando do diretório:" //Printa o texto entre parênteses
+pwd //Printa o caminho referente ao diretório atual
