2d1431fdd5d6c04072064d66c34ee60eab49496c
Pedro Nieto <pedronieto.2005@gmail.com>
2022-07-12 08:28:52 -0300

    Update Perfil.sh

--- Perfil.sh
+++ Perfil.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 clear #Limpa o terminal
-echo "Seu nome de usuário é:" #Printa o texto entre parênteses
+echo "Seu nome de usuário é:" #Printa no terminal o texto entre aspas.
 whoami #Printa o nome de usuário
-echo "" #Printa o texto entre parênteses
+echo "" #Printa no terminal o texto entre aspas.
 
-echo "Info de dia e hora atual:" #Printa o texto entre parênteses
+echo "Info de dia e hora atual:" #Printa no terminal o texto entre aspas.
 date #Printa a data e a hora
-echo "" #Printa o texto entre parênteses
+echo "" #Printa no terminal o texto entre aspas.
 
-echo "O script está executando do diretório:" #Printa o texto entre parênteses
+echo "O script está executando do diretório:" #Printa no terminal o texto entre aspas.
 pwd #Printa o caminho referente ao diretório atual
